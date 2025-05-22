c3fe5a799f7eb16099474a8c7d508d4e8b0d84bf
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2025-05-22 19:10:16 +0530

    HTTP Client Utility

---
+++ app/utils/http_client.py
@@ -0,0 +1,35 @@
+import http.client
+import json
+from app.config import Config
+
+def make_video_api_request(payload_data):
+    conn = http.client.HTTPSConnection(Config.VIDEO_API_HOST)
+    
+    payload = json.dumps(payload_data)
+    
+    headers = {
+        'x-rapidapi-key': Config.VIDEO_API_KEY,
+        'x-rapidapi-host': Config.VIDEO_API_HOST,
+        'Content-Type': "application/json"
+    }
+    
+    conn.request("POST", "/generateScenePrompts?mood=dramatic&style=cinematic&language=en&noqueue=1", payload, headers)
+    
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
+
+def make_summary_api_request(url):
+    conn = http.client.HTTPSConnection(Config.SUMMARY_API_HOST)
+    
+    headers = {
+        'x-rapidapi-key': Config.SUMMARY_API_KEY,
+        'x-rapidapi-host': Config.SUMMARY_API_HOST
+    }
+    
+    endpoint = f"/summarize?url={url}&lang=en&engine=2"
+    conn.request("GET", endpoint, headers=headers)
+    
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
